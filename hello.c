4495941f460b5554e075c89ac331b958cf36b8ba
Nandhini Lakuduva <Nandhini@wireless-10-148-66-105.public.utexas.edu>
2016-09-01 17:12:38 -0500

    added hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+/*
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello, world!\n");
+    return 0;
+}
+*/
+
+//C hello world example
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    printf("Nandhini is great\n");
+    printf("testing\n");
+    return 0;
+}
